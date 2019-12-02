eddcc7680daf1e8ce296351fce692916561931c0
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2019-12-03 00:10:45 +0100

    middleware_group_added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+
+        'auth' => [
+            \App\Http\Middleware\CheckUser::class,
+        ]
     ];
 
     /**
