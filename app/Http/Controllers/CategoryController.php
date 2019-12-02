01c7f8cf7813951ee0814eb53015385017b3d7cf
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2019-12-02 17:51:04 +0100

    category_registration_completed

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -34,7 +35,15 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $category = new Category();
+        $category->name = $request->name;
+        //$category->save();
+
+        return response()->json([
+
+            "name" => $category->name,
+            
+        ], 200);
     }
 
     /**
