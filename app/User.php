9d6f00b86c78ec1405c7f185b31b374298b6f06e
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2019-12-02 18:54:39 +0100

    user_has_many_relationship_created

--- app/User.php
+++ app/User.php
@@ -8,4 +8,12 @@ class User extends Model
 {
     protected $table = 'users'; 
     protected $fillable = ['id', 'name', 'email', 'password'];
+
+    public function categories()
+    {
+
+        return $this->hasMany(Category::class);
+
+    }
+
 }
