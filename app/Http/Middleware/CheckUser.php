32efc794db7093142d42094e2ee54c4c9fc75196
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2019-12-02 22:45:00 +0100

    middleware_class_created

---
+++ app/Http/Middleware/CheckUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\User;
+use Closure;
+
+class CheckUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        
+        
+        
+        
+        
+        
+        
+        
+        return $next($request);
+    }
+}
