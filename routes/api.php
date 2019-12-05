8060a0846605ff3c212e9264c9b2e41203db88a5
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2019-12-05 20:04:02 +0100

    show_passwords_with_categories_route_added

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::ApiResource('/users', 'UserController');
     Route::ApiResource('/categories', 'CategoryController');
     Route::get('/show_categories', 'CategoryController@show_categories');
+    Route::get('/show_passwords_with_categories', 'CategoryController@show_passwords_with_categories');
     Route::ApiResource('/passwords', 'PasswordController');
     Route::get('/show_passwords', 'PasswordController@show_passwords');
 
